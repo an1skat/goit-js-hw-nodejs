835d147076c94579051981e1f3fb4cfcfb3c4480
an1skat <156462349+an1skat@users.noreply.github.com>
2025-02-15 13:19:32 +0200

    finish hw

---
+++ main.js
@@ -0,0 +1,17 @@
+const data = [];
+const form = document.querySelector("[data-form]");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const email = e.target.querySelector("#email").value;
+  const pass = e.target.querySelector("#password").value;
+
+  if (data.find((user) => user.email === email)) {
+    return alert("This email is already taken");
+  }
+
+  data.push({
+    email: email,
+    password: pass,
+  });
+});
